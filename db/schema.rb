16e249bdcc2ec4fd0b22b1da073a85ba93d44f68
Andrew Schnieder <andrewschnieder@gmail.com>
2015-03-17 20:05:10 -0700

    migrate buttons, add button links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317225438) do
+ActiveRecord::Schema.define(version: 20150318014258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20150317225438) do
     t.integer  "question_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "upvote"
+    t.integer  "downvote"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
@@ -31,6 +33,8 @@ ActiveRecord::Schema.define(version: 20150317225438) do
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "upvote"
+    t.integer  "downvote"
   end
 
   add_foreign_key "answers", "questions"
